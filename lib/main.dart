672e048922de77356f1a42214077ab2f8cff220d
Ting-Ting Chen <hchunyu.tty@gmail.com>
2025-02-26 15:34:04 +0000

    Add a new page

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
       case 0:
         page = GeneratorPage();
       case 1:
-        page = Placeholder();
+        page = FavouritesPage();
       default:
         throw UnimplementedError('no widget for $selectedIndex');  
     }
@@ -100,6 +100,37 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         );
       }
+    );
+  }
+}
+
+class FavouritesPage extends StatelessWidget {
+  // const FavouritesPage({
+  //   super.key,
+  // });
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favourites.isEmpty) {
+      return Center(
+        child: Text('No favourites yet.')
+      );
+    }
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+                '${appState.favourites.length} favouries:')
+          ),
+          for (var pair in appState.favourites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
